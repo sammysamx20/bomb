baff968e89af6903c48e59d735f77a9dac5f2674
Sam Young <youngsam@oregonstate.edu>
2021-01-15 01:48:40 -0800

    Fdsa

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import { Fireworks } from 'fireworks/lib/react'
+import pic1 from './pics/IMG_8784.JPG'
+import pic5 from './pics/lp_image.jpg'
+import pic2 from './pics/lp_image (1).jpg'
+import pic3 from './pics/lp_image (2).jpg'
+import pic4 from './pics/lp_image (3).jpg'
+import bomb from './pics/bomb.png'
+
+
+
+function App() {
+const [yes, setYes] = useState(false)
+  let fxProps1 = {
+    count: 3,
+    interval: 200,
+    colors: ['#cc3333', '#4CAF50', '#81C784'],
+    calc: (props, i) => ({
+      ...props,
+      x: (i + 1) * (window.innerWidth / 3) - (i + 1) * 100,
+      y: 200 + Math.random() * 100 - 50 + (i === 2 ? -80 : 0)
+    })
+  }
+
+  return (
+    <div className="App">
+      {yes &&
+      <div>
+       <img src={bomb} className="App-logo" alt="logo" />
+    <Fireworks {...fxProps1} />  
+    <div>SHE ALWAYS IS</div> </div>  }
+    <button style={{backgroundColor: "#008CBA", padding: "15px 32px"}} onClick={() => setYes(!yes)}>She bomb?</button>
+
+      <header className="App-header">
+        <img src={pic1} className="App-logo" alt="logo" />
+        <img src={pic2} className="App-logo" alt="logo" />
+        <img src={pic3} className="App-logo" alt="logo" />
+        <img src={pic4} className="App-logo" alt="logo" />
+        <img src={pic5} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
